ae47b49b1fb8d100592d36eaef91d75c21251dc2
harshaMatha <mathaharsha@gmail.com>
2023-06-04 18:26:01 +0530

    First Commit

---
+++ Inventory Management/Inventory management/src/main/dal/InfoGetterInterface.java
@@ -0,0 +1,9 @@
+package main.dal;
+
+import java.util.List;
+
+import main.models.Employee;
+
+public interface InfoGetterInterface {
+	List<Employee> getEmployeeData();
+}
---
+++ Inventory Management/Inventory management/src/main/models/Employee.java
@@ -0,0 +1,22 @@
+package main.models;
+
+public class Employee {
+	int empno;
+	String ename;
+
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+}
